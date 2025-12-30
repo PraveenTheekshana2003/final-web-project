dd4c1702213f9156aa9f5fa149690db208e08a7b
Praveen Theekshana <praveentheekshana03@gmail.com>
2025-12-31 03:22:18 +0530

    top rated books, image corousel added

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ const savedColorScheme = localStorage.getItem('color-scheme');
 if(savedColorScheme === 'dark') {
     toggle.checked = true;
     document.documentElement.style.colorScheme = 'dark';
-}
-else 
+} else {
     document.documentElement.style.colorScheme = 'light';
+}
 
 toggle.addEventListener('change', function() {
     if (this.checked) {
@@ -16,4 +16,38 @@ toggle.addEventListener('change', function() {
         document.documentElement.style.colorScheme = 'light';
         localStorage.setItem('color-scheme','light');
     }
+});
+
+
+const carousels = document.querySelectorAll('.book-carousel');
+
+carousels.forEach((carousel) => {
+    const track = carousel.querySelector('.track');
+    const next = carousel.querySelector('.next');
+    const prev = carousel.querySelector('.prev');
+    const cards = track.querySelectorAll('.book-card');
+    let index = 0;
+    const cardWidth = cards[0].offsetWidth + 20;
+
+    next.addEventListener('click', () => {
+        if(index < cards.length - 1) index++;
+        track.style.transform = `translateX(-${index * cardWidth}px)`;
+    });
+
+    prev.addEventListener('click', () => {
+        if(index > 0) index--;
+        track.style.transform = `translateX(-${index * cardWidth}px)`;
+    });
+    
+    document.addEventListener('keydown', (e) => {
+        if(carousel.getBoundingClientRect().top < window.innerHeight && carousel.getBoundingClientRect().bottom > 0) {
+            if(e.key === 'ArrowRight') {
+                if(index < cards.length - 1) index++;
+                track.style.transform = `translateX(-${index * cardWidth}px)`;
+            } else if(e.key === 'ArrowLeft') {
+                if(index > 0) index--;
+                track.style.transform = `translateX(-${index * cardWidth}px)`;
+            }
+        }
+    });
 });
